f7df36c59ed9ea7478253e6e21ff70e5794372fe
Timothy Oliver <oliverwakogi@live.com>
2019-10-14 17:34:33 +0300

    complete source page

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from . import main
-from ..requests import get_sources
+from ..requests import get_sources, get_articles_from_source
 
 #views
 @main.route('/')
@@ -12,3 +12,13 @@ def index():
   news_sources = get_sources()
 
   return render_template('index.html', news_sources=news_sources)
+
+@main.route('/source/<source_name>')
+def source(source_name):
+  '''
+  features story and returns source.html and its respective articles
+  '''
+
+  articles = get_articles_from_source(source_name)
+
+  return render_template('source.html', articles=articles)
