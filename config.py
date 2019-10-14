29439f53f13c9b02dbc58b7abce7bd84a0589d39
Timothy Oliver <oliverwakogi@live.com>
2019-10-14 10:46:06 +0300

    change MOVIE_API_KEY to  NEWS_API_KEY

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
   general configuration parent class
   '''
 
-  MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
+  NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
   SECRET_KEY = os.environ.get('SECRET_KEY')
 
   pass
